9db93949d3e661ec6ef237a83df70da7a7917b0d
Fedosova Vikrtoria <vik.fed2110@gmail.com>
2021-11-29 21:21:00 +0300

    Add Lab4

--- Lab4/HomeWorks/ClassTest.cs
+++ Lab4/HomeWorks/ClassTest.cs
@@ -9,7 +9,10 @@ namespace Lab4.HomeWorks
     {
         public static void MetrodWithException(int temp) 
         {
-            
+            if(temp==10 || temp==4 || temp == 99)
+            {
+                throw new Exception();
+            }   
         }
     }
 }
